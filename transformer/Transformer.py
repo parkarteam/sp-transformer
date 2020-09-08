ee2491cdd9727e69d9e5deb6b9821b315ffed15b
Kiran Kumar <gmkumar2005@gmail.com>
2020-09-08 13:32:26 +0000

    Addd logs

--- transformer/Transformer.py
+++ transformer/Transformer.py
@@ -1,11 +1,11 @@
 import numpy as np
-
+import click
 class Transformer():
     def __init__(self):
     	print(" Starting transformer...")
 
-
-
     def transform_input(self, text_msg, feature_names=None):
-               return np.array([text_msg[0]])
+            click.echo('inside transform_input text_msg: %s' %text_msg)    
+            click.echo('inside transform_input processed value: %s' %np.array([text_msg[0]]))    
+            return np.array([text_msg[0]])
 
